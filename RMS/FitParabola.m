71f8694723dc2c744b2bab715834fbaf8eda336f
vignats <salome.vi38@gmail.com>
2024-03-15 10:14:17 +0100

    Modif 14

--- RMS/FitParabola.m
+++ RMS/FitParabola.m
@@ -1,4 +1,4 @@
-function [surface] = FitParabola(binary_image, boundary_endost_init)
+function [surface] = FitParabola(binary_image, boundary_endost_init, print)
 % FITPARABOLA Fits a parabolic curve to the provided boundary points of the endosteum.
 %
 %   surface = FitParabola(binary_image, boundary_endost_init) fits a parabolic curve to
@@ -22,14 +22,16 @@ function [surface] = FitParabola(binary_image, boundary_endost_init)
     surface = [boundary_endost_init(:, 1), y];     % Associate the Y coordinate of the parabola with the X coordinate
     
     % Plot final curve
-    figure
-    imshow(binary_image);
-    hold on;
-    plot(boundary_endost_init(:, 1), boundary_endost_init(:, 2), 'b.');  % Trace le contour
-    hold on;
-    plot(boundary_endost_init(:, 1), y, 'r-');  % Trace la parabole ajustée
-    xlabel('X');
-    ylabel('Y');
-    legend('Contour', 'Parabole');
-    title('Fitted parabola to the endost boundary')
+    if print
+        figure
+        imshow(binary_image);
+        hold on;
+        plot(boundary_endost_init(:, 1), boundary_endost_init(:, 2), 'b.');  % Trace le contour
+        hold on;
+        plot(boundary_endost_init(:, 1), y, 'r-');  % Trace la parabole ajustée
+        xlabel('X');
+        ylabel('Y');
+        legend('Contour', 'Parabole');
+        title('Fitted parabola to the endost boundary')
+    end
 end
--- RMS/GetBoneRms.m
+++ RMS/GetBoneRms.m
@@ -3,7 +3,7 @@ addpath(genpath('~/Documents'));
 % Pathway to the bone image to process
 bone = {'271G' '267G' '251G' '245D' '227G'};
 bone_nb = 5;
-dirname = ['~/Documents/BONE_STRUCTURE/', bone{bone_nb}, '/'];
+dirname = ['~/Documents/BoneRugosity/RMS/', bone{bone_nb}, '/'];
 num_slice = 1226;
 file = ['SAMPLE_', bone{bone_nb}, '_SLICE_', int2str(num_slice), '.bmp']; 
 filename = [dirname , file];
@@ -11,30 +11,37 @@ filename = [dirname , file];
 % Load image
 bone_bmp = imread(filename); 
 
-%% THRESHOLD TO CONVERT TO BINARY IMAGE
+%% INITIAL COMPUTATION OF RMS HEIGTH
+% THRESHOLD TO CONVERT TO BINARY IMAGE
 threshold = graythresh(bone_bmp); % Find an automatic threshold
 binary_image_init = imbinarize(bone_bmp, threshold);
 
 % Plot to verifiy
-figure,
-subplot(2,1,1);
-imshow(bone_bmp); 
-title('Original bone image');
-subplot(2,1,2);
-imshow(binary_image_init); 
-title(sprintf('Binary bone image with a %0.1f threshold ', threshold));
-
-%% BOUNDARIES COMPUTATION
+print = false;
+if print 
+    figure,
+    subplot(2,1,1);
+    imshow(bone_bmp); 
+    title('Original bone image');
+    subplot(2,1,2);
+    imshow(binary_image_init); 
+    title(sprintf('Binary bone image with a %0.1f threshold ', threshold));
+end 
+% BOUNDARIES COMPUTATION
 [boundary_endost_init, xy] = ExtractBoundary(binary_image_init);
 
-%% POLYNOMIAL FITTING IF THE ENDOST
-surface = FitParabola(binary_image_init, boundary_endost_init);
+% POLYNOMIAL FITTING IF THE ENDOST
+surface = FitParabola(binary_image_init, boundary_endost_init, true);
 
-%% COMPUTE RMS HEIGTH 
+% COMPUTATION OF RMS 
+rms_0 = rms(surface(:, 2) - boundary_endost_init(:, 2));
+%% COMPUTE RMS HEIGTH FOR ALL IMAGES
+% Compute the rms for all the other images, using the parabola and endost
+% limitation defined for the first image 
 tic
 disp('------- Computation of the mean rms -------')
 % Initialise the rms_mean with the rms of the first image
-rms_mean = [rms(surface(:, 2) - boundary_endost_init(:, 2))];
+rms_all = [num_slice, rms_0];
 
 % To start the loop with the second image, change filename to point it to
 % the second image
@@ -42,8 +49,6 @@ num_slice = num_slice + 100;
 file = ['SAMPLE_', bone{bone_nb}, '_SLICE_', int2str(num_slice), '.bmp']; 
 filename = [dirname , file];
 
-% Compute the rms for all the other images, using the parabola and endost
-% limitation defined for the first image 
 while exist(filename, 'file')
     % Load image
     bone_bmp = imread(filename); 
@@ -60,13 +65,17 @@ while exist(filename, 'file')
     % The xy limits obtained with the first image need to be indicated for
     % the other images
     [boundary_endost, ~] = ExtractBoundary(binary_image, xy);
+    surface = FitParabola(binary_image_init, boundary_endost, false);
     
     % Compute the rms
     [~, idx_surface, idx_boundary_endost] = intersect(surface(:, 1), boundary_endost(:, 1));
-    rms_mean(end +1) = rms(surface(idx_surface, 2) - boundary_endost(idx_boundary_endost, 2));
+    rms_all(end +1,:) = [num_slice, rms(surface(idx_surface, 2) - boundary_endost(idx_boundary_endost, 2))]*9e-3;       % Rms (mm) - Pixel size = 9e-3 mm
 
     num_slice = num_slice + 1;
     file = ['SAMPLE_', bone{bone_nb}, '_SLICE_', int2str(num_slice), '.bmp']; 
     filename = [dirname , file];
 end
-toc
+toc 
+
+%% SAVE RMS VALUE IN .mat FILE
+save(fullfile(dirname, 'rms.mat'), "rms_all");
